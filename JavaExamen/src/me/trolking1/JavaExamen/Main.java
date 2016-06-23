c377ed891c57a4a9e6d3e40a35a66cabed7b463c
Gabriel D'Hondt <gabriel.dhondt@hotmail.com>
2016-06-23 12:19:19 +0200

    Examen is gemaakt en klaar om te laten invullen.

--- JavaExamen/src/me/trolking1/JavaExamen/Main.java
+++ JavaExamen/src/me/trolking1/JavaExamen/Main.java
@@ -1,6 +1,39 @@
 package me.trolking1.JavaExamen;
 
 public class Main {
+	
+	/* Opdracht 1)
+	 * Maak een variabel string en print die als 1 ste in de console
+	 * 
+	 * Opdracht 2)
+	 * Maak 3 variabel die een integer returnen. Zorg voor 2 cijfers en 1 totaal
+	 * en maak een if statement die kijkt of integer 1 groter is als integer 2
+	 * als dat zo is print het totaal ander print je een message.
+	 * 
+	 * Opdracht 3)
+	 * Maak 2 variabels in een method die een integer returnt. 
+	 * Print het grootste cijfer uit gebruik om de grootste integer te vinden de math class van java.
+	 * zorg ervoor dat het cijfer word uitgeprint
+	 * 
+	 * Opdracht 4)
+	 * Maak een method die een iput heeft van een string en die een string returnt.
+	 * zorg ervoor dat die string word uitgeprint via die method.
+	 * 
+	 * Opdracht 5)
+	 * Maak een class met een constructor met een input van een naam een een leeftijd. 
+	 * Zorg ervoor dat ik die gegevens kan ophalen door getters en setters.
+	 * Maak een Array in de main class en zorg ervoor dat er 2 mensen in zitten met hun leeftijd.
+	 * Laat dat de arraylist uitvoeren in de console met behulp van een for loop.
+	 * 
+	 * Opdracht 5)
+	 * Hoe maak je een constructor.
+	 * 
+	 * Opdracht 6)
+	 * Wat is verschil tussen public een static.
+	 * 
+	 * Opdracht 7)
+	 * Hoe maak je een method die iets returnt.
+	*/
 
 	public static void main(String[] args) {
 		
